f34400fa06d0ac75372573fb132adbb5f51cbbc3
mohamed <mohamedatef224455@gmail.com>
2022-02-14 19:23:40 +0200

    deploy

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const morgan = require("morgan");
-require('dotenv').config();
-const connectDb = require('./config/db.config');
-const projectRoute = require('./routes/project.routes');
-const taskRoute = require('./routes/task.routes');
+require("dotenv").config();
+const connectDb = require("./config/db.config");
+const projectRoute = require("./routes/project.routes");
+const taskRoute = require("./routes/task.routes");
+const cors = require("cors");
 
 //our server
 const app = express();
+//enable cors
+app.use(cors());
 //DB connection
 connectDb();
 
 //middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
-app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
 
 //routes
-app.use('/project' , projectRoute);
-app.use('/task' , taskRoute);
+app.use("/project", projectRoute);
+app.use("/task", taskRoute);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
